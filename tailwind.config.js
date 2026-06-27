a7a81603804774e6dd66f44662c294a0cb0b23d0
Muhammad Zaheeb <zaheeb.qureshi@gmail.com>
2026-06-27 13:45:46 +0500

    fix tailwind v3 stable setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 export default {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
